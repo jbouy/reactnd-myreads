af59b2ead4033bd9ea6da03aea40da20d3f320b6
James Bouy <jbouy@turner-industries.com>
2018-10-03 08:29:26 -0500

    feat: add routing to App

---
+++ src/screens/BookShelvesScreen.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class BookShelvesScreen extends Component {
+  render() {
+    return <div>BookShelvesScreen</div>;
+  }
+}
+
+export default BookShelvesScreen;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class SearchScreen extends Component {
+  render() {
+    return <div>SearchScreen</div>;
+  }
+}
+
+export default SearchScreen;
